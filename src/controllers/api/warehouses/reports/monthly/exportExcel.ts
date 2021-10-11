0bf591d1728eead98463cdffc349c64578a84180
Wisnu <wisnuaryadipa93@gmail.com>
2021-10-11 13:50:26 +0700

    Add export excel for report monthly warehouse

--- src/controllers/api/warehouses/reports/monthly/exportExcel.ts
+++ src/controllers/api/warehouses/reports/monthly/exportExcel.ts
@@ -1,13 +1,17 @@
 import {Request, Response} from 'express';
 import {BaseController} from '@src/controllers/baseController'
 import logicController from '@src/controllers/logic'
+import fs from 'fs';
 
 
 class Controller extends BaseController {
 
     requestHandler = async (req: Request, res: Response) => {
+
+        let readFile = fs.readFileSync('./export/report/v2/monthlyReport.json')
+        const file = JSON.parse(readFile.toString());
         
-        const data = logicController.warehouses.report.monthly.exportExcel.index('reportMonth.xlsx');
+        const data = logicController.warehouses.report.monthly.exportExcel.index(file);
         this.sendResponse(req, res, {data});
     }
 
--- src/controllers/logic/warehouses/report/monthly/exportExcel.ts
+++ src/controllers/logic/warehouses/report/monthly/exportExcel.ts
@@ -1,7 +1,33 @@
-import xlsx, {Workbook} from 'exceljs';
+import xlsx, {Workbook, Worksheet} from 'exceljs';
 import moment from 'moment';
+import fs from 'fs';
+import _ from 'lodash';
 
 class Controller {
+
+    currentRow: number;
+
+    constructor() {
+        this.currentRow = 0;
+    }
+
+    addCurrRow = (): void => {
+        this.currentRow += 1;
+    }
+    
+    index = (data: any) => {
+
+        const timeNow = moment().format('DD-MM-YYYY');
+        let wb: Workbook = new xlsx.Workbook();
+        
+        wb = this.initial(wb);
+        let firstSheet: Worksheet = wb.addWorksheet('Montly');
+        firstSheet = this.createFieldColumn(firstSheet);
+        firstSheet = this.addData(data, firstSheet);
+
+        wb.xlsx.writeFile('reportMonth '+timeNow+'.xlsx')
+
+    }
 
     initial = (workbook: Workbook) => {
 
@@ -11,22 +37,76 @@ class Controller {
         return workbook;
     }
 
+    createFieldColumn = (workSheet: Worksheet) => {
 
+        // MERGING FIRST ROW HEADER
+        workSheet.mergeCells(1,1,2,1);
+        workSheet.mergeCells(1,2,2,2);
+        workSheet.mergeCells(1,3,1,4);
+        workSheet.mergeCells(1,5,1,6);
+        workSheet.mergeCells(1,7,1,8);
+        workSheet.mergeCells(1,9,2,9);
+        
+        // ADD VALUES FIRST ROW HEADER
+        this.addCurrRow();
+        workSheet.getCell(this.currentRow,1).value = "Item";
+        workSheet.getCell(this.currentRow,2).value = "Periode";
+        workSheet.getCell(this.currentRow,3).value = "Kedatangan";
+        workSheet.getCell(this.currentRow,5).value = "Posisi Stok";
+        workSheet.getCell(this.currentRow,7).value = "Pemakaian";
+        workSheet.getCell(this.currentRow,9).value = "Keterangan";
 
-    index = (filename: string, data?: any) => {
-
-        let wb: Workbook = new xlsx.Workbook();
-        wb = this.initial(wb);
-        const firstSheet = wb.addWorksheet('Montly');
-        firstSheet.columns = [
-            { header: 'Id', key: 'id', width: 10 },
-            { header: 'Name', key: 'name', width: 32 },
-            { header: 'D.O.B.', key: 'DOB', width: 10, outlineLevel: 1 }
-        ];
-
-        wb.xlsx.writeFile('reportMonth.xlsx')
+        // ADD VALUES SECOND ROW HEADER
+        this.addCurrRow();
+        workSheet.getCell(this.currentRow,3).value = "Tgl. Datang";
+        workSheet.getCell(this.currentRow,4).value = "Jml. Datang";
+        workSheet.getCell(this.currentRow,5).value = "TGL 20";
+        workSheet.getCell(this.currentRow,6).value = "Akhir Bulan";
+        workSheet.getCell(this.currentRow,7).value = "Jml. Pakai";
+    
+        return workSheet;
 
     }
+
+    addData = (data: any, workSheet: Worksheet) => {
+
+        data.forEach((subJenisItem: any) => {
+            
+            subJenisItem.items.forEach((item:any) => {
+                for (const yearKey in item.report) {
+                    this.addCurrRow();
+                    workSheet.mergeCells(this.currentRow,1,this.currentRow,9);
+                    workSheet.getCell(this.currentRow,1).value = subJenisItem.sub_jenis_item;
+                    workSheet.getCell(this.currentRow,1).alignment = { vertical: 'middle', horizontal: 'center'}
+                    this.addCurrRow();
+                    workSheet.mergeCells(this.currentRow,1,this.currentRow,9);
+                    workSheet.getCell(this.currentRow,1).value = item.nama_item;
+                    workSheet.getCell(this.currentRow,1).alignment = { vertical: 'middle', horizontal: 'center'}
+                    this.addCurrRow();
+                    workSheet.mergeCells(this.currentRow,1,this.currentRow,9);
+                    workSheet.getCell(this.currentRow,1).value = yearKey;
+                    workSheet.getCell(this.currentRow,1).alignment = { vertical: 'middle', horizontal: 'center'}
+                    for (const monthKey in item.report[yearKey]) {
+                        this.addCurrRow();
+                        workSheet.getCell(this.currentRow,1).value = subJenisItem.sub_jenis_item + " - " + item.nama_item;
+                        workSheet.getCell(this.currentRow,2).value = monthKey;
+                        workSheet.getCell(this.currentRow,4).value = item.report[yearKey][monthKey].totalQuantity.recivedGoods;
+                        workSheet.getCell(this.currentRow,5).value = item.report[yearKey][monthKey].totalQuantity.StockAt20th;
+                        workSheet.getCell(this.currentRow,6).value = item.report[yearKey][monthKey].totalQuantity.StockendOfMonth;
+                        workSheet.getCell(this.currentRow,7).value = item.report[yearKey][monthKey].totalQuantity.usedGoods;
+                        
+                    }
+                }
+                
+            })
+        })
+
+        return workSheet;
+    } 
+
+
+
+
 } 
 
 export default new Controller();
